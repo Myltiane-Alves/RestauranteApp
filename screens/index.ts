fa90e7631b0be31d9f177de56bf317ae16e19d0d
Myltiane <myltiane.aux@gmail.com>
2022-12-20 21:14:48 -0300

    feat criando context

---
+++ screens/index.ts
@@ -0,0 +1,11 @@
+export enum Screen  {
+    Home = 'Home',
+    Contact = 'Contact',
+    Menu = 'Menu'
+}
+
+export const Screens = {
+    [Screen.Home]: undefined,
+    [Screen.Contact]: undefined,
+    [Screen.Menu]: undefined
+}
