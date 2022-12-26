14868a88c29b3f139d521fd42a8706059e632793
Myltiane <myltiane.aux@gmail.com>
2022-12-26 20:11:43 -0300

    feat: diciando datepicker

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -168,7 +168,29 @@ const Header = () => {
         <button type="submit">
           Reservar
         </button>
-      </div> */}
+      </div> 
+      
+      
+      
+        const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomTimeInput = ({ date, value, onChange }) => (
+    <input
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      style={{ border: "solid 1px pink" }}
+    />
+  );
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      showTimeInput
+      timeInputLabel="Time:"
+      customTimeInput={<ExampleCustomTimeInput />}
+      highlightDates={[subDays(new Date(), 7), addDays(new Date(), 
+      />
+  );
+      */}
 
 
 
